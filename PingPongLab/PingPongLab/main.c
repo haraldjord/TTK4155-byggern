c698ed879b816c24f0940cfbf8a26a879bf1fab5
Christoffer Mehren <chrimeh@ntnu.no>
2022-09-01 09:27:56 +0200

    added project file:

---
+++ PingPongLab/PingPongLab/main.c
@@ -0,0 +1,18 @@
+/*
+ * PingPongLab.c
+ *
+ * Created: 01.09.2022 09:26:13
+ * Author : chrimeh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
